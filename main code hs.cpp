6685c6f1e946d73bc4a38f4f63ad3cfef253c315
NAGITO-KOMAEDA <78807301+NAGITO-KOMAEDA@users.noreply.github.com>
2025-05-24 14:49:42 +0300

    Add files via upload

---
+++ main code hs.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <windows.h>
+
+const int NOTUSED = system("color 05");
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "RUS");
+	std::cout << "я ещё ничё не сделала нормального, не ругайтесь пжшка Т^Т";
+}
